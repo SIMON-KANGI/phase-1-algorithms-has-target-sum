c559793f2ae5cdabf071dccbb6847033ceb25716
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-01-16 07:22:42 +0300

    completed the test

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if(array[i] + array[j] === target) {
+        return true
+      }
+      }
+    }
+    return false
+  }
 
+console.log(hasTargetSum([2,3,5,6],8))
 /* 
   Write the Big O time complexity of your function here
+
+  The Big O notation is O(n^2)
 */
 
 /* 
   Add your pseudocode here
+
+  if  sum of array index 1 and array index 2 is equal to target
+  return true
+  if not return false
 */
 
 /*
   Add written explanation of your solution here
+Iterate through the whole array using a for loop.
+Add every pair of the numbers in the array
 */
 
 // You can run `node index.js` to view these console logs
